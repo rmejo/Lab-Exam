962fb3f21b5a9ca23ca890138d685487b3eda156
rmejo <106475814+rmejo@users.noreply.github.com>
2022-05-29 15:35:12 +0300

    Create javaHashtable.java

---
+++ javaHashtable.java
@@ -0,0 +1,15 @@
+import java.util.Hashtable;
+
+public class javaHashtable {
+
+	public static void main(String[] args) {
+		Hashtable<String, Integer> myHT = new Hashtable<>(); // TODO 1
+		myHT.put("Khalid", 5); // TODO 2
+		myHT.put("Usman", 6); // TODO 3
+		myHT.put("Jones", 7); // TODO 4
+		myHT.remove("Usman"); // TODO 5
+		myHT.put("Saleem", 7); // TODO 6
+		System.out.println(myHT); // TODO 7
+	}
+
+}
